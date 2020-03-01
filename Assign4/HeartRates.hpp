a21fdcf91f8024d414a82260e34a801db5caf99b
Andrew Jowe <andrewjowe@yahoo.com>
2020-03-01 14:38:25 -0800

    assign4

--- Assign4/HeartRates.hpp
+++ Assign4/HeartRates.hpp
@@ -10,9 +10,10 @@ class HeartRates {
     int mo_birth, day_birth, yr_birth;
   public:
     HeartRates(string _first_name, string _last_name, int _mo_birth, int _day_birth, int _yr_birth);
-    int getAge();
-    double getMaximumHeartRate();
-    double getTargetHeartRate();
+    int getAge(int current_mo, int current_day, int current_yr);
+    int getMaximumHeartRate(int current_mo, int current_day, int current_yr);
+    string getTargetHeartRate(int current_mo, int current_day, int current_yr);
+    void doSomethingForConstructor(); //DELETE
 };
 
 #endif //HEARTRATES_HPP
--- Assign4/assign4.cpp
+++ Assign4/assign4.cpp
@@ -5,17 +5,28 @@
 #include <iostream>
 
 int main () {
-  string first_name, last_name;
+  string first_name, last_name, buf;
   cout << "Please enter first and last name (separated by spaces):" << endl;
   cin >> first_name >> last_name;
   int month, day, year;
   cout << "Please enter month, day, and year of birth (seperated by spaces):" << endl;
-  cin >> month >> day >> year;
-  //Create new class object
+  cin >> buf;
+  month = atoi(buf.c_str()); //Getting input as string and then converting to an int so the program does not crash if user inputs letters as int
+  cin >> buf;
+  day = atoi(buf.c_str());
+  cin >> buf;
+  year = atoi(buf.c_str());
+  HeartRates heartRates = HeartRates(first_name, last_name, month, day, year);
   cout << "Please enter today's month, day, and year (separated by spaces):" << endl;
-  cin >> month >> day >> year;
-  //Call getAge method
-  //Call other methods
+  cin >> buf;
+  month = atoi(buf.c_str());
+  cin >> buf;
+  day = atoi(buf.c_str());
+  cin >> buf;
+  year = atoi(buf.c_str());
+  cout << "Current Age: " << heartRates.getAge(month, day, year) << endl;
+  cout << "Max Heart Rate: " << heartRates.getMaximumHeartRate(month, day, year) << endl;
+  cout << "Target Heart Rate: " << heartRates.getTargetHeartRate(month, day, year) << endl;
   return 0;
 }
 

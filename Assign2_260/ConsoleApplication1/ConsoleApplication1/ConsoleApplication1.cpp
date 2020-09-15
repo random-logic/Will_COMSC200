9a8bdc5073ff07bdce6fde18e5eaedb6ea19cc3b
Andrew Jowe <ajowe034@insite.4cd.edu>
2020-09-15 16:24:49 -0700

    Assign2_260

--- Assign2_260/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ Assign2_260/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -18,69 +18,68 @@ using namespace std;
 
 int bin_to_dec(string);
 string dec_to_bin(int);
-int hex_to_dec(string);  
-string dec_to_hex(int);  
+int hex_to_dec(string);
+string dec_to_hex(int);
 
 
-int main() 
+int main()
 {
-	cout.flush();
-	
-	cout<<"10000011 binary = "<<bin_to_dec("10000011")<<" decimal\n"; // you should get 131
+	cout << "10000011 binary = " << bin_to_dec("10000011") << " decimal\n"; // you should get 131
 	cout<<"01010101 binary = "<<bin_to_dec("01010101")<<" decimal\n"; // you should get 85
 	cout<<"1111111111111111 binary = "<<bin_to_dec("1111111111111111")<<" decimal\n"; // you should get 65,535
 	cout<<"0111111111111111 binary = "<<bin_to_dec("0111111111111111")<<" decimal\n\n"; // you should get 32,767
-	
+
 	cout<<"255 decimal = "<<dec_to_bin(255)<<" binary\n"; //you should get 11111111
 	cout<<"65532 decimal = "<<dec_to_bin(65532)<<" binary\n"; //you should get 1111111111111100
 	cout<<"12 decimal = "<<dec_to_bin(12)<<" binary\n"; //you should get 1100
 	cout<<"1000000 decimal = "<<dec_to_bin(1000000)<<" binary\n\n"; //you should get 11110100001001000000
-	
+
 	cout<<"ABC hexadecimal = "<<hex_to_dec("ABC")<<" decimal\n"; //you should get 2,748
 	cout<<"F5 hexadecimal = "<<hex_to_dec("F5")<<" decimal\n"; //you should get 245
 	cout<<"1234 hexadecimal = "<<hex_to_dec("1234")<<" decimal\n"; //you should get 4,660
 	cout<<"FDECB hexadecimal = "<<hex_to_dec("FDECB")<<" decimal\n\n"; //you should get 1,040,075
-	
+
 	cout<<"512 decimal = "<<dec_to_hex(512)<<" hexadecimal\n"; //you should get 200
 	cout<<"5000 decimal = "<<dec_to_hex(5000)<<" hexadecimal\n"; //you should get 1388
 	cout<<"900000 decimal = "<<dec_to_hex(900000)<<" hexadecimal\n"; //you should get DBBA0
 	cout<<"65525 decimal = "<<dec_to_hex(65525)<<" hexadecimal\n\n"; //you should get FFF5
-	
-	
-	system("PAUSE");
+
+
+	//system("PAUSE");
 	return 0;
 
 }
 
 // Converts any UNsigned binary number to decimal
 
-int bin_to_dec(string s) 
+int bin_to_dec(string s)
 {
+	string str(s);
 	int number = 0;
 
 	int str_index = 0;
-	for (int i = s.length() - 1; i >= 0; i++) {
-		if (s.at(str_index) == '1')
+	for (int i = str.length() - 1; i >= 0; i--) {
+		if (str.at(str_index) == '1')
 			number += pow(2, i);
 
 		str_index++;
 	}
 
 	return number;
-}	
+}
 
 //Converts any non-negative decimal number to binary
 
-string dec_to_bin(int n)  
+string dec_to_bin(int n)
 {
 	int str_len = 1;
 
-	while (pow(2, str_len + 1) < n)
+	while (pow(2, str_len) < n)
 		str_len++;
 
 	string str = "";
 
-	for (int i = str_len - 1; i >= 0; i++) {
+	for (int i = str_len - 1; i >= 0; i--) {
 		if (n >= pow(2, i)) {
 			str += "1";
 			n -= pow(2, i);
@@ -96,13 +95,14 @@ string dec_to_bin(int n)
 
 //Converts any UNsigned hexadecimal number to decimal
 
-int hex_to_dec(string s) 
+int hex_to_dec(string s)
 {
+	string str(s);
 	int number = 0;
 
 	int str_index = 0;
-	for (int i = s.length() - 1; i >= 0; i++) {
-		string digit_str(1, s.at(str_index));
+	for (int i = str.length() - 1; i >= 0; i--) {
+		string digit_str(1, str.at(str_index));
 		str_index++;
 
 		int digit;
@@ -116,7 +116,7 @@ int hex_to_dec(string s)
 			digit = 13;
 		else if (digit_str == "E")
 			digit = 14;
-		else if (digit_str == "F") 
+		else if (digit_str == "F")
 			digit = 15;
 		else
 			digit = atoi(digit_str.c_str());
@@ -125,12 +125,12 @@ int hex_to_dec(string s)
 	}
 
 	return number;
-}	
+}
 
 
 //Converts any non-negative decimal number to hexadecimalConverts any non-negative decimal number to hexadecimal
 
-string dec_to_hex(int n)  
+string dec_to_hex(int n)
 {
 	string str = "";
 
@@ -147,7 +147,7 @@ string dec_to_hex(int n)
 			str = "D" + str;
 		else if (number == 14)
 			str = "E" + str;
-		else if (number == 15) 
+		else if (number == 15)
 			str = "F" + str;
 		else
 			str = to_string(number) + str;
